3387696d4f012be8cd0344c0c82b2cddcfbb0066
Roberto Nascimento <robertok3r@gmail.com>
2021-11-18 13:10:17 -0300

    Adicionando projeto SOLYD

---
+++ MinicursoPythonSOLYD/Exercicio-Aula 5 - Estruturas de laço (WHILE e FOR).py
@@ -0,0 +1,7 @@
+'''
+ Faça um programa que leia a quantidade de pessoas que
+serão convidadas para uma festa.
+Após isso o programa irá perguntar o nome de todas as pessoas e colocar numa lista de convidados
+Após isso irá imprimir todos os nomes da lista
+'''
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from turtle import clear
+nome = input(('Digite seu nome: '))
+idade = input(('Digite sua Idade: '))
+cpf = input('Digite seu CPF: ')
+print(nome,'\n',idade,'\n',cpf)
+
