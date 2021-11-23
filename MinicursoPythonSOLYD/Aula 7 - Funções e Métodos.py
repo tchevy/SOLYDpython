fa44723fceddcaf580a00e65b127ae4e180b5f19
Roberto Nascimento <robertok3r@gmail.com>
2021-11-23 13:17:35 -0300

    Aula 7

---
+++ MinicursoPythonSOLYD/Aula 7 - Funções e Métodos.py
@@ -0,0 +1,10 @@
+#print() ---> imprime o conteudo na tela
+#len() ---> infora a quantidade de caracter, é usado junto com print Ex. print(len("Olá Mundo"))
+print(len ("Olá Mundo"))
+# Podemos fazer nossas proprias funçoes
+# Ex.
+def soma(numero1, numero2):
+    resp = numero1 + numero2
+    return resp
+resposta = soma(256, 478)
+print(resposta)
