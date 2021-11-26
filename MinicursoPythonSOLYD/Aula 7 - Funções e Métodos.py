a44367d9d5181ffed8fa8ac3599b8c1c421f69b8
Roberto Nascimento <robertok3r@gmail.com>
2021-11-26 13:11:06 -0300

    Aulas 7, 8 e

--- MinicursoPythonSOLYD/Aula 7 - Funções e Métodos.py
+++ MinicursoPythonSOLYD/Aula 7 - Funções e Métodos.py
@@ -8,3 +8,28 @@ def soma(numero1, numero2):
     return resp
 resposta = soma(256, 478)
 print(resposta)
+
+def imprime_oi():
+    print("OI TUDO BEM?")
+
+imprime_oi()
+imprime_oi()
+imprime_oi()
+
+
+def tem_sete_letras(objeto):
+    if len(objeto) == 7:
+        return True
+    else:
+        return False
+if tem_sete_letras('Roberto'):
+    print("Realmente tem sete caracter")
+else:
+    print("Não tem sete caracter")
+
+
+'''
+EXERCICIO: Escreva uma funçao que receba 
+um objeto de coleção e retorna o valor 
+do maior numero dentro dessa coleção
+'''
