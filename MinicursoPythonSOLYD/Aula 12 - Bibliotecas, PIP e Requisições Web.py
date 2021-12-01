5744d28117282e60a8d5c7158154c3b613d41ddb
Roberto Nascimento <robertok3r@gmail.com>
2021-12-01 16:51:11 -0300

    Aula 12 01-12-2021 as 16:51

--- MinicursoPythonSOLYD/Aula 12 - Bibliotecas, PIP e Requisições Web.py
+++ MinicursoPythonSOLYD/Aula 12 - Bibliotecas, PIP e Requisições Web.py
@@ -1,4 +1,21 @@
-import sys
-import time
 import requests
-requests.get( )
+
+                        #import Beautiful Soup 4 BS4 pip install bs4
+texto = None
+try:
+    requisicao = requests.get('https://putsreq.com/LZKDmO54nggSWu1c302a')
+    texto = requisicao.text
+except Exception as e:
+    print("A requisição deu erro:", e )
+
+print(texto)
+
+'''
+    requisicao = requests.get('https://putsreq.com/HWON7wBhaWD4dJ1EP7RZ')
+    print(requisicao.text)
+    requisicao2 = requests.post('https://www.seduc.ce.gov.br/')
+    print(type(requisicao))
+    print(type(requisicao2))
+    print(requisicao.status_code) # recebe o status da requisção
+'''
+
