36c009e58d15fd58bececd4fd2c3ff2c8481b24d
Roberto Nascimento <robertok3r@gmail.com>
2021-12-02 16:14:40 -0300

    Aula 13 02-12-2021 as 16:14

---
+++ MinicursoPythonSOLYD/Aula 13 - API, JSON e consultando listas de filmes.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def requisicao(titulo):
+     try:
+       req = requests.get('http://img.omdbapi.com/?apikey=[3bb7744d]&' + titulo)
+       dicionario = json.loads(req.text)
+       return dicionario
+     except:
+       print('Erro na conexão')
+       return None
+
+def printar_detalhes(filme):
+     print('Título:', filme['Title'])
+     print('Ano:', filme['Year'])
+     print('Diretor:', filme['Director'])
+     print('Atores:', filme['Actors'])
+     print('Nota:', filme['imdbRating'])
+
+sair = False
+while not sair:
+    op = input('Escreva o nome de um filme ou SAIR para fechar:')
+    if op == 'SAIR':
+        sair = True
+        print('Saindo...')
+else:
+     filme = requisicao(op)
+     if filme['Response'] == False:
+         print('Filme nao encontrado')
+     else:
+         printar_detalhes(filme)
+
+#print(req.text)
