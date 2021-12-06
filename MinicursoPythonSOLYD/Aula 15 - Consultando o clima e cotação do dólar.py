496d4cb9e869f49cf54c6b386374d022de8aa69c
Roberto Nascimento <robertok3r@gmail.com>
2021-12-06 14:38:58 -0300

    Aula 15,16 inicio

--- MinicursoPythonSOLYD/Aula 15 - Consultando o clima e cotação do dólar.py
+++ MinicursoPythonSOLYD/Aula 15 - Consultando o clima e cotação do dólar.py
@@ -1,4 +1,15 @@
 #COTAÇÂO DO DOLLAR
 import requests
-requisicao = requests.get('https://api.hgbrasil.com/finance/stock_price?key=908b0482&symbol=bidi4   908b0482&symbol=bidi4')
-print(requisicao.text)
+import json
+#requisicao = requests.get('https://api.hgbrasil.com/finance/stock_price?key=908b0482&symbol=bidi4   908b0482&symbol=bidi4')
+#print(requisicao.text)
+"""
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=VCJF25RNXTCWPFUU'
+r = requests.get(url)
+data = r.json()
+print(data)
+"""
+
+requisicao = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=VCJF25RNXTCWPFUU')
+cotacao = json.loads(requisicao.text)
+print(cotacao)
