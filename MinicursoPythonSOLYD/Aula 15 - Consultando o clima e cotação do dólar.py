c414407548b7f11f287899809e8b7081f5ee8590
Roberto Nascimento <robertok3r@gmail.com>
2021-12-07 07:52:56 -0300

    Aula 15 atualização de chave

--- MinicursoPythonSOLYD/Aula 15 - Consultando o clima e cotação do dólar.py
+++ MinicursoPythonSOLYD/Aula 15 - Consultando o clima e cotação do dólar.py
@@ -4,12 +4,12 @@ import json
 #requisicao = requests.get('https://api.hgbrasil.com/finance/stock_price?key=908b0482&symbol=bidi4   908b0482&symbol=bidi4')
 #print(requisicao.text)
 """
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=VCJF25RNXTCWPFUU'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=minhachave'
 r = requests.get(url)
 data = r.json()
 print(data)
 """
 
-requisicao = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=VCJF25RNXTCWPFUU')
+requisicao = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=minhachave')
 cotacao = json.loads(requisicao.text)
 print(cotacao)
